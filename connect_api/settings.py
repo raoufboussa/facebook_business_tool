9b4145ce6a40726fffbbed1647e6b9a6c8ace254
raoufboussa <fa_boussa@esi.dz>
2020-07-29 17:08:08 +0100

    replace staticfiles by static

--- connect_api/settings.py
+++ connect_api/settings.py
@@ -132,15 +132,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/img/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,"media/img")  
-
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
